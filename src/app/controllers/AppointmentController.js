6a3dedb6abde55afa1b11560fd7601d071b10f6e
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-07-12 13:43:19 -0300

    Add AppointmentController Index method

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,39 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+    async index(req, res) {
+        const { page = 1 } = req.query;
+        const appointments = await Appointment.findAll({
+            where: {
+                user_id: req.userId,
+                canceled_at: null,
+            },
+            order: ['date'],
+            attributes: ['id', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+                },
+            ],
+        });
+        return res.json(appointments);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             provider_id: Yup.number().required(),
@@ -15,20 +46,42 @@ class AppointmentController {
 
         const { provider_id, date } = req.body;
 
-        const isProvider = await User.findOne({
+        const checkIsProvider = await User.findOne({
             where: { id: provider_id, provider: true },
         });
 
-        if (!isProvider) {
+        if (!checkIsProvider) {
             return res.status(401).json({
                 error: 'You can only create appointments with providers.',
             });
         }
 
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past dates are not permited' });
+        }
+
+        const checkDateAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkDateAvailability) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
+        }
+
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            date,
+            date: hourStart,
         });
 
         return res.json(appointment);
--- src/routes.js
+++ src/routes.js
@@ -15,9 +15,14 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
