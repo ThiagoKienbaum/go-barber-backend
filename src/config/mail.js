183664b3990e3ea7f1ffa31383932c71240c622d
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-07-12 19:47:31 -0300

    Add nodemail and layouts

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '7fd045efff7faa',
+        pass: '881fecade26384',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
